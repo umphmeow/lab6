8204bfd4bded6e714a4b9443be5a9dfd65345457
Ilya K <131490186+umphmeow@users.noreply.github.com>
2023-05-25 19:49:39 +0400

    Update main_v2.py

--- main_v2.py
+++ main_v2.py
@@ -27,57 +27,61 @@ def get_permutations(books, r):
     return permutations
 
 k = int(input("Введите количество книг: "))
-books = [{'title': f"Book{i+1}"} for i in range(k)]
-
-all_book_sequences = read_books(books, k)
-print("Все возможные последовательности книг:")
-for sequence in all_book_sequences:
-    print([book['title'] for book in sequence])
-
-books = rating_and_pages(books)
-
-print("Список книг с присвоенными рейтингом и количеством страниц:")
-for book in books:
-    print(book)
-
-books = [book for book in books if 100 <= book['pages'] <= 300]
-
-if books:
-    min_rating_book = min(books, key=lambda x: x['rating'])
+if k == 0:
+    print('Нельзя составить последовательности чтения книг')
 else:
-    min_rating_book = None
-max_rating_book = max(books, key=lambda x: x['rating'])
-
-sequences = []
-for i in range(2, len(books) + 1):
-    for sequence in get_permutations(books, i):
-        sequence = list(sequence)
-        if min_rating_book in sequence:
-            sequence.remove(min_rating_book)
-            sequence.append(min_rating_book)
-        if max_rating_book in sequence:
-            sequence.remove(max_rating_book)
-            sequence.insert(1, max_rating_book)
-        for j in range(2, len(sequence)):
-            if sequence[j] in [min_rating_book, max_rating_book]:
-                continue
-            for k in range(j-1, 0, -1):
-                if sequence[k] in [min_rating_book, max_rating_book]:
+    books = [{'title': f"Book{i+1}"} for i in range(k)]
+    
+    all_book_sequences = read_books(books, k)
+    print("Все возможные последовательности чтения книг:")
+    for sequence in all_book_sequences:
+        print([book['title'] for book in sequence])
+    
+    books = rating_and_pages(books)
+    
+    print("Список книг с присвоенными рейтингом и количеством страниц:")
+    for book in books:
+        print(book)
+    
+    books = [book for book in books if 100 <= book['pages'] <= 300]
+    
+    if books:
+        min_rating_book = min(books, key=lambda x: x['rating'])
+        max_rating_book = max(books, key=lambda x: x['rating'])
+    else:
+        min_rating_book = None
+    
+    
+    sequences = []
+    for i in range(2, len(books) + 1):
+        for sequence in get_permutations(books, i):
+            sequence = list(sequence)
+            if min_rating_book in sequence:
+                sequence.remove(min_rating_book)
+                sequence.append(min_rating_book)
+            if max_rating_book in sequence:
+                sequence.remove(max_rating_book)
+                sequence.insert(1, max_rating_book)
+            for j in range(2, len(sequence)):
+                if sequence[j] in [min_rating_book, max_rating_book]:
                     continue
-                if sequence[k]['rating'] < sequence[j]['rating']:
-                    sequence[k+1:j+1] = sequence[k:j]
-                    sequence[k] = sequence[j]
-                    break
-        if sequence not in sequences:
-            sequences.append(sequence)
-
-print("Уникальные возможные последовательности книг после фильтрации:")
-for sequence in sequences:
-    avg_rating = sum([book['rating'] for book in sequence]) / len(sequence)
-    print([book['title'] for book in sequence], f"Средний рейтинг: {avg_rating:.2f}")
-
-if sequences:
-    max_avg_rating = max([sum([book['rating'] for book in sequence]) / len(sequence) for sequence in sequences])
-    print("Максимальный средний рейтинг:", max_avg_rating)
-else:
-    print("Список последовательностей пустой, невозможно вычислить максимальный средний рейтинг.")
+                for k in range(j-1, 0, -1):
+                    if sequence[k] in [min_rating_book, max_rating_book]:
+                        continue
+                    if sequence[k]['rating'] < sequence[j]['rating']:
+                        sequence[k+1:j+1] = sequence[k:j]
+                        sequence[k] = sequence[j]
+                        break
+            if sequence not in sequences:
+                sequences.append(sequence)
+    
+    print("Уникальные возможные последовательности книг для чтения после фильтрации:")
+    for sequence in sequences:
+        avg_rating = sum([book['rating'] for book in sequence]) / len(sequence)
+        print([book['title'] for book in sequence], f"Средний рейтинг: {avg_rating:.2f}")
+    
+    if sequences:
+        max_avg_rating = max([sum([book['rating'] for book in sequence]) / len(sequence) for sequence in sequences])
+        print("Максимальный средний рейтинг:", max_avg_rating)
+    else:
+        print("В списке недостатоно книг, невозможно вычислить максимальный средний рейтинг.")
